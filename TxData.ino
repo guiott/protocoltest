f80183a370697059e27d5fdb18770a3fec997e86
guiott <guiott@61727698-3b9f-110e-05af-d211b3550c95>
2013-08-30 07:34:08 +0000

    Added more security options. Added shutdown procedure. Added some more rx-tx parameters exchange. Optimization of rx-tx procedures. This version matches the r21 of lino-console (http://code.google.com/p/lino-console/source/detail?r=21)

--- TxData.ino
+++ TxData.ino
@@ -11,16 +11,19 @@ void TxData(char TxCmd, int TxCmdLen)
 {// Send the requested parameters, with the correct handshake
     byte TxChkSum=0;
     
-    TxBuff[0] = HEADER;
-    TxBuff[1] = ID;           // often not used, useful if many device on bus
-    TxBuff[2] = TxCmd;        // command
-    TxBuff[3] = TxCmdLen - RX_HEADER_LEN;     // lenght of the payload plus CHKSUM
-    for (int i=0; i<=TxCmdLen; i++)
-    {// compute the checksum while sending
-      TxChkSum += (byte)(TxBuff[i]);
-      Serial.write(TxBuff[i]);
+    if(PushOk)  // pushing the button simulate a TX error
+    {
+      TxBuff[0] = HEADER;
+      TxBuff[1] = ID;           // often not used, useful if many device on bus
+      TxBuff[2] = TxCmd;        // command
+      TxBuff[3] = TxCmdLen - RX_HEADER_LEN;     // lenght of the payload plus CHKSUM
+      for (int i=0; i<=TxCmdLen; i++)
+      {// compute the checksum while sending
+        TxChkSum += (byte)(TxBuff[i]);
+        Serial.write(TxBuff[i]);
+      }
+     
+      Serial.write(TxChkSum);
     }
-   
-    Serial.write(TxChkSum);
 }
 
